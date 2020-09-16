797369e3927f574744cccfeb9a71630bb82c22e6
minsub <aka.tomochan@gmail.com>
2020-09-16 11:19:52 +0900

    run script edit

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+echo "> 키파일 복사"
+mkdir ~/app/dist/server/lib/keys
+cp ~/configs/config.js ~/app/dist/server/config.js
+cp ~/configs/emailKey ~/app/dist/server/lib/keys/emailKey
+cp ~/configs/private ~/app/dist/server/lib/keys/private
+cp ~/configs/public ~/app/dist/server/lib/keys/public
+cp ~/configs/recaptch ~/app/dist/server/lib/keys/recaptch
+cp ~/configs/tokenKey ~/app/dist/server/lib/keys/tokenKey
 echo "> pm2 삭제"
 pm2 stop pinventory
 echo "> pm2 시작"
